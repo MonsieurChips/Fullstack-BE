db9758899592b37d3616c6ce9043d80e6bf3de2d
Kennedy Michael <initeyen@gmail.com>
2025-07-14 19:02:51 +0400

    commit13

--- server.js
+++ server.js
@@ -6,16 +6,13 @@ require("dotenv").config();
 const app = require("./app");
 const { connectToDb } = require("./src/utils/database");
 
-// Get the port from environment variables, with a default of 3000
+
 const PORT = process.env.PORT || 3000;
 
-/**
- * Main function to start the server.
- * It first connects to the database and then starts the Express app.
- */
+
 async function startServer() {
   try {
-    // Establish connection to the database
+
     await connectToDb();
     console.log("Successfully connected to MongoDB Atlas!");
 
