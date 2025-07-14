f5c994fa40c7b449c5d4e0abf229214f027630f3
Kennedy Michael <initeyen@gmail.com>
2025-07-14 19:03:15 +0400

    commit14

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
-// server.js
-
-// Load environment variables from .env file
 require("dotenv").config();
 
 const app = require("./app");
 const { connectToDb } = require("./src/utils/database");
 
-
 const PORT = process.env.PORT || 3000;
-
 
 async function startServer() {
   try {
-
     await connectToDb();
     console.log("Successfully connected to MongoDB Atlas!");
 
-    // Start listening for requests
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error("Failed to start the server:", error);
-    // Exit the process with an error code if the database connection fails
+
     process.exit(1);
   }
 }
 
-// Execute the server start function
 startServer();
